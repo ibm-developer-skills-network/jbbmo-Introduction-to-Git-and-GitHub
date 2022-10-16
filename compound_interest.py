ecd9f837f0626094b31af4425e1d459e9fcc116a
Sobirjon Kholiqov <84468754+Sobirjon2222@users.noreply.github.com>
2022-10-16 18:47:51 +0200

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -1,7 +1,7 @@
 # This script calculates yearly compound interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: Sobirjon Kholiqov
 
 # Input:
 # p, principal amount
