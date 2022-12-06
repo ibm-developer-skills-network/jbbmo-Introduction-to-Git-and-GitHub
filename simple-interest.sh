5edc7bb9f4ab352234f2be1e5be40fceefc3453f
mikepoullas <mikepoullas@yahoo.com>
2022-12-06 15:01:27 -0500

    fixed my username

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <your Github username>
+# Addtional Authors: Mike Poullas (1)
+# mikepoullas
 
 # Input:
 # p, principal amount
