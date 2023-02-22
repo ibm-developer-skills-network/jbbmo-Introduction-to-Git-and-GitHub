49b1a937f0a8cecdfb1df68b810a214238cfa2b5
Carlos MZ <44822033+carlos18mz@users.noreply.github.com>
2023-02-21 19:43:42 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+# Carlos18mz
+
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
+
+# Output:
+# simple interest = p*t*r
+
 echo"Enter the principal:"
 read p    
 echo"Enter rate of interest per year:"
