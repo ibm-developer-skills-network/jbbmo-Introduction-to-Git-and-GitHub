a7083799b386231313322d36b4130257aade9992
Dhanalakshmi-lab-stack <132611225+Dhanalakshmi-lab-stack@users.noreply.github.com>
2023-05-11 01:45:11 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,8 @@
 
 # Author: Upkar Lidder (IBM)
 # Addtional Authors:
-# <your Github username>
+# Dhanalakshmi-lab-stack
+
 
 # Input:
 # p, principal amount
@@ -21,6 +22,6 @@ read r
 echo "Enter time period in years:"
 read t
 
-s=$(expr $p \* $t \* $r / 100)
+s=$(expr $p * $t * $r)
 echo "The simple interest is: "
 echo $s
