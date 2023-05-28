6033b10abeb6d447b0f8cae13e1c7513e2d6c99b
mariiarubiin <134617905+mariiarubiin@users.noreply.github.com>
2023-05-28 16:44:39 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,9 +2,9 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: Maria rubin
 # Addtional Authors:
-# <your Github username>
+# <mariiarubin>
 
 # Input:
 # p, principal amount
