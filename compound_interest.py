6d6ed02a830695dab18f710055aa738dd2f9d2f8
Jarunwebb1 <137920658+Jarunwebb1@users.noreply.github.com>
2023-06-28 14:09:02 +0100

    Update compound_interest.py
    This is for the peers review

--- compound_interest.py
+++ compound_interest.py
@@ -1,4 +1,4 @@
-# This script calculates yearly compound interest given principal, annual rate of interest and time period in years.
+# This script calculates yearly compound interest given principal, annual rate of interest, and time period in years.
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
@@ -11,14 +11,15 @@
 # Output:
 # compound interest = p * (1 + r/100)^t
 
-
 def compound_interest(p, t, r):
     return p * (pow((1 + r / 100), t))
 
-
 if __name__ == "__main__":
-    p = float(input("Enter the principal amount: "))
-    t = float(input("Enter the time period: "))
-    r = float(input("Enter the rate of interest: "))
+    try:
+        p = float(input("Enter the principal amount: "))
+        t = float(input("Enter the time period: "))
+        r = float(input("Enter the rate of interest: "))
 
-    print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
+        print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
+    except ValueError:
+        print("Invalid input. Please enter numeric values for principal amount, time period, and rate of interest.")
