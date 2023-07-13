0f704a37da661317b6bdae1de408f9632faedcf0
Micael VERDIEU <63418147+M1cael@users.noreply.github.com>
2023-07-12 21:47:05 -0400

    correction

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
-
+#
 # Author: Upkar Lidder (IBM)
 # Addtional Authors:
 # <your Github username>
