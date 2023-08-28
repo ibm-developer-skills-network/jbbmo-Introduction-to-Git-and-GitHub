8ec4f3c54a21bc02c7148b178959b7f08798b624
Alexis Estévez <58530844+adestevez@users.noreply.github.com>
2023-08-28 10:03:39 -0500

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -18,7 +18,10 @@ def compound_interest(p, t, r):
 
 if __name__ == "__main__":
     p = float(input("Enter the principal amount: "))
+    
     t = float(input("Enter the time period: "))
+    
     r = float(input("Enter the rate of interest: "))
+    
 
     print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
