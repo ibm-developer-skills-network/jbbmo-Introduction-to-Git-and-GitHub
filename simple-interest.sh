a1462f319b21b730aa0664e0aaf5934ca1638d2c
NimiIsaiah <141259138+NimiIsaiah@users.noreply.github.com>
2023-09-05 15:09:01 +0100

    Update simple-interest.sh
    Author change

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,9 +2,9 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: Nimi Isaiah (IBM)
 # Addtional Authors:
-# <your Github username>
+# <NimiIsaiah>
 
 # Input:
 # p, principal amount
