99a0a0addbc7ca2a7aa55f70e07f7b14872d716b
Taimoor Amin <90872946+CaptainTaimoor@users.noreply.github.com>
2023-09-15 00:14:35 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,7 +2,7 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: CaptainTaimoor
 # Addtional Authors:
 # <your Github username>
 
