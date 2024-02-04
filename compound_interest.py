782d7ee1f519fc509df1e246211983de9c29f2fe
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2024-02-04 18:43:41 +0500

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -1,4 +1,4 @@
-# This script calculates yearly compound interest given principal, annual rate of interest and time period in years.
+# This script calculates yearly compound interest given principal, annual rate of interest, and time period in years.
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
@@ -9,12 +9,10 @@
 # r, annual rate of interest
 
 # Output:
-# compound interest = p * (1 + r/100)^t
-
+# compound interest = p * (1 + r/100)**t
 
 def compound_interest(p, t, r):
-    return p * (pow((1 + r / 100), t))
-
+    return p * (1 + r / 100) ** t
 
 if __name__ == "__main__":
     p = float(input("Enter the principal amount: "))
